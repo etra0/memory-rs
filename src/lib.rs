88b3c701dd1e4dc9769665ffabfa15a2b152578d
Sebastián Aedo <saedo@alumnos.inf.utfsm.cl>
2020-04-20 11:04:34 -0400

    Now all test are passing

--- src/lib.rs
+++ src/lib.rs
@@ -24,32 +24,58 @@ mod tests {
 
     #[test]
     fn test_yakuza() {
+        use winapi::um::processthreadsapi::OpenProcess;
         let process_id = super::process::get_process_id("Yakuza0.exe")
             .expect("No process found");
-        let module_base_address = super::process::get_module_base(process_id, "Yakuza0.exe")
+        let module_base_address = super::process::get_module_base(process_id,
+            "Yakuza0.exe")
             .expect("No module found");
 
         assert_eq!(module_base_address, 0x140000000);
         
         let offset = 0x18B1E5;
 
-        let nops = vec![0x90, 0x90, 0x90];
+        let nops = vec![0x90, 0x90, 0x90, 0x90];
         let mut orig: Vec<u8> = Vec::new();
 
-        println!("Process id: {:x}, Module_base_address: {:x}", process_id, module_base_address);
+        println!("Process id: {:x}, Module_base_address: {:x}",
+            process_id, module_base_address);
 
-        let h_process = unsafe { winapi::um::processthreadsapi::OpenProcess(winapi::um::winnt::PROCESS_ALL_ACCESS, false as i32, process_id) };
+        let h_process = unsafe {
+            OpenProcess(
+                winapi::um::winnt::PROCESS_ALL_ACCESS,
+                false as i32,
+                process_id
+            )
+        };
+
         assert!(!h_process.is_null());
 
         println!("hprocess: {:x?}", h_process);
 
-        super::memory::get_aob(h_process, module_base_address + offset, &mut orig, 3);
+        super::memory::get_aob(
+            h_process,
+            module_base_address + offset,
+            &mut orig,
+            3
+        );
 
         println!("vec: {:x?}", orig);
 
-        super::memory::write_aob(h_process, module_base_address + offset, &nops);
+        let written = super::memory::write_aob(
+            h_process,
+            module_base_address + offset,
+            &nops
+        );
 
-        super::memory::get_aob(h_process, module_base_address + offset, &mut orig, 3);
+        assert_eq!(nops.len(), written, "Incorrect number of written bytes");
+        orig = Vec::new();
+        super::memory::get_aob(
+            h_process,
+            module_base_address + offset,
+            &mut orig,
+            nops.len()
+        );
         assert_eq!(orig, nops, "The memory was not written");
 
     }
--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -1,12 +1,12 @@
 use winapi::shared::basetsd::{DWORD_PTR};
 use winapi::um::handleapi;
-use winapi::shared::minwindef::{LPVOID, LPCVOID, DWORD};
+use winapi::shared::minwindef::{LPVOID, LPCVOID, DWORD, PDWORD};
 use winapi::um::winnt::{HANDLE, PAGE_EXECUTE_READWRITE};
 use winapi::um::tlhelp32;
 use winapi::um::memoryapi::{ReadProcessMemory, WriteProcessMemory, VirtualProtectEx};
 use std::mem;
 
-pub fn get_aob(h_process: HANDLE, ptr: DWORD_PTR, target: &mut Vec<u8>, n: i32) {
+pub fn get_aob(h_process: HANDLE, ptr: DWORD_PTR, target: &mut Vec<u8>, n: usize) {
     let mut c_addr = ptr;
     let mut c_value: u8 = 0x0;
     for i in 0..n {
@@ -24,17 +24,39 @@ pub fn get_aob(h_process: HANDLE, ptr: DWORD_PTR, target: &mut Vec<u8>, n: i32) 
     }
 }
 
-pub fn write_aob(h_process: HANDLE, ptr: DWORD_PTR, source: &Vec<u8>) {
+pub fn write_aob(h_process: HANDLE, ptr: DWORD_PTR, source: &Vec<u8>) -> usize {
     let mut protection_bytes: DWORD = 0x0;
     let mut c_addr = ptr;
     let size = source.len();
-    let arr = [0x90, 0x90, 0x90];
+    let mut written = 0;
 
     println!("writing into {:x}", ptr);
 
     unsafe { 
-        VirtualProtectEx(h_process, (&mut c_addr as *mut DWORD_PTR) as LPVOID, size, PAGE_EXECUTE_READWRITE, &mut protection_bytes);
-        WriteProcessMemory(h_process, (&mut c_addr as *mut DWORD_PTR) as LPVOID, arr.as_ptr() as LPVOID, size, std::ptr::null_mut());
-        VirtualProtectEx(h_process, (&mut c_addr as *mut DWORD_PTR) as LPVOID, size, protection_bytes, &mut protection_bytes);
+        VirtualProtectEx(
+            h_process,
+            c_addr as LPVOID,
+            size,
+            PAGE_EXECUTE_READWRITE,
+            &mut protection_bytes as PDWORD
+        );
+
+        WriteProcessMemory(
+            h_process,
+            c_addr as LPVOID,
+            source[..].as_ptr() as LPVOID,
+            size,
+            &mut written
+        );
+
+        VirtualProtectEx(
+            h_process,
+            c_addr as LPVOID,
+            size,
+            protection_bytes,
+            &mut protection_bytes as PDWORD
+        );
     }
+
+    written
 }
